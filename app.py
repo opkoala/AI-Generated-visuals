6a522164037f3eb46aad4026560d1c8caef25df9
opkoala <kingdragon_usa@yahoo.com>
2023-05-19 11:09:41 -0700

    Updates

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect, send_file, session
 from pytube import YouTube
 from io import BytesIO
+import librosa
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         session['link'] = request.form.get('url')
@@ -14,10 +15,10 @@ def home():
             url.check_availability()
         except:
             return render_template("error.html")
-        return render_template("download.html", url = url)
+        return render_template("download.html", url=url)
     return render_template("home.html")
 
-@app.route("/download", methods = ["GET", "POST"])
+@app.route("/download", methods=["GET", "POST"])
 def download_video():
     if request.method == "POST":
         buffer = BytesIO()
@@ -26,6 +27,11 @@ def download_video():
         video = url.streams.get_by_itag(itag)
         video.stream_to_buffer(buffer)
         buffer.seek(0)
+        
+        # Process the audio using librosa
+        audio_data, sample_rate = librosa.load(buffer)
+        # Perform further processing or analysis on the audio
+        
         return send_file(buffer, as_attachment=True, download_name="Video - YT2Video.mp4", mimetype="video/mp4")
     return redirect(url_for("home"))
 
